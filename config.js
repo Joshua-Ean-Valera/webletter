55a02f7f355b01a5280604d917f4dc1d295cd8ec
Joshua-Ean <jovalera@my.cspc.edu.ph>
2026-05-19 13:24:14 +0800

    sss

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyChlqPz19QeeImmQSt4oWdAluD6XbWch8g",
+  authDomain: "webletter-7f64a.firebaseapp.com",
+  databaseURL: "https://webletter-7f64a-default-rtdb.firebaseio.com",
+  projectId: "webletter-7f64a",
+  storageBucket: "webletter-7f64a.firebasestorage.app",
+  messagingSenderId: "316842154834",
+  appId: "1:316842154834:web:e88b9335861ca97f8f388b"
+};
